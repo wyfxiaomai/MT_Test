ee4bcb87a4cfe6db2e91db1c4f2c567fbb0f1d09
wyfxiaomai <1508430602@qq.com>
2018-04-13 13:47:07 +0800

    修改添加获取系统路径问题

--- test/RunCase/RunCase_Single.py
+++ test/RunCase/RunCase_Single.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- encoding:UTF-8 -*-
 import unittest,time,os,sys
-#rootpath=str('/Web_MaiTianOnLineAutoTest/')
-rootpath = os.path.dirname(os.path.dirname(__file__))
+rootpath=str('/Web_MaiTianOnLineAutoTest/')
+#rootpath = os.path.dirname(os.path.dirname(__file__))
 syspath=sys.path
 sys.path=[]
 sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
